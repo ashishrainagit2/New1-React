4bc7dc7de6096f36a33bdc8693dc5ab2db58326c
Ashish Raina <ashish.raina@bookmyshow.com>
2020-08-19 07:46:45 +0530

    added buggy counter

--- src/containers/ErrorComponent/ErrorComponent.js
+++ src/containers/ErrorComponent/ErrorComponent.js
@@ -1,16 +1,25 @@
 import React, {Component} from "react";
 import ErrorBoundary from "../../hoc/ErrorBoundary";
 
-export class ErrorComponent extends Component {
+class ErrorComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter: 0};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(({counter}) => ({
+      counter: counter + 1,
+    }));
+  }
+
   render() {
-    return (
-      <ErrorBoundary>
-        <div>
-          <div>broken div </div>
-          {/* --no-verify  */}
-        </div>
-      </ErrorBoundary>
-    );
+    if (this.state.counter === 5) {
+      // Simulate a JS error
+      throw new Error("I crashed!");
+    }
+    return <h1 onClick={this.handleClick}>{this.state.counter}</h1>;
   }
 }
 
